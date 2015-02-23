e50f1e89c61ea6a60d6b4b938d7f8a654c96db04
sevidmusic <sevi@moviemeetinghouse.com>
2015-02-22 21:41:51 -0500

    Modified the rest of the contentManager forms to incorporate SDM Assembler's incorporateAppOutput() to output their respective forms.

--- core/apps/contentManager/forms/contentManagerSelectThemeForm.php
+++ core/apps/contentManager/forms/contentManagerSelectThemeForm.php
@@ -36,4 +36,4 @@ $editcontentform->form_elements = array(
 
 $editcontentform->__build_form($sdmcore->getRootDirectoryUrl());
 // add form to content
-$sdmassembler_dataObject->content->$sdmassembler_requestedpage->main_content .= '<!-- contentManager Edit Content Form -->' . $editcontentform->__get_form() . '<!-- End contentManager Edit Content Form -->';
+$sdmassembler->incorporateAppOutput($sdmassembler_dataObject, '<!-- contentManager Edit Content Form -->' . $editcontentform->__get_form() . '<!-- End contentManager Edit Content Form -->');
