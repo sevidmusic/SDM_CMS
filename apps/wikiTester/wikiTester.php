ce54f562bdbe36150c6d4da9a9eb440be9304600
sevidmusic <sevi@moviemeetinghouse.com>
2015-03-31 20:01:27 -0400

    wikiTester: appropriate query is being made, still need to sort through data

--- apps/wikiTester/wikiTester.php
+++ apps/wikiTester/wikiTester.php
@@ -6,18 +6,26 @@
 $output = '<p>This app performs tests to see what wikipedia returns for agiven movie title.</p>';
 require($sdmcore->sdmCoreGetUserAppDirectoryPath() . '/wikiTester/functions.php');
 // args
-$post = array(
+$revisionPost = array(
     'action' => 'query',
-    'format' => 'json',
     'prop' => 'revisions',
-    'titles' => 'goodfellas',
+    'titles' => 'goodfellas|family guy',
     'rvprop' => 'content',
+    'rvsection' => 0, // important | gets the infobox
+//    'rvexpandtemplates' => 'rvexpandtemplates', // changes how the returned page items are fomratted. i.e., if this property does NOT exist then pure wikitext is returend, if it is present then wikitext formatted for readability is returned. THIS SHOULD NOT BE USED outside of development as it will make it impossible to parse the returned data efficiently.
+    'redirects' => 'redirects', // important | reolves any redirects related to the titles
+    'format' => 'json', // important | we need json
+    'indexpageids' => 'indexpageids', // important | returns an array of all the page ids for the returned pages
+//    'continue' => '||',
 );
+$post = $revisionPost;
+//$post = $expandTemplatesPost;
+//$post = $extractPost;
 $i = 0;
 $postArgsCount = count($post);
 $get = '';
 foreach ($post as $key => $value) {
-    $get .= $key . '=' . $value . ($i === ($postArgsCount - 1) ? '' : '&');
+    $get .= $key . '=' . str_replace(' ', '%20', $value) . ($i === ($postArgsCount - 1) ? '' : '&');
     $i++;
 }
 // build api request url
@@ -25,6 +33,18 @@ $apiRequestUrl = 'http://en.wikipedia.org/w/api.php?' . $get;
 // parse request
 $apiRequest = $sdmcore->sdmCoreCurlGrabContent($apiRequestUrl);
 // parse output
-$output .= '<div style="height:420px;overflow:auto;"><h3>Wiki Tester : Expand Templates Example</h3><p>Tested Url : ' . $apiRequestUrl . '</p><p>Tested $_POST values : ' . wikiArrayToList($post) . '</p>' . wikiArrayToList(json_decode($apiRequest, TRUE)) . '</div>';
+switch ($_GET['wikiquerymode']) {
+    case 'wikidata':
+        $output .= '<div style="height:420px;overflow:auto;"><h3>Wiki Tester : Query Example</h3><p>Tested Url : ' . '<a href="' . $apiRequestUrl . '">' . $apiRequestUrl . '</a>' . '</p><p>Tested $_POST values : ' . wikiArrayToList($post, 'POST') . '</p>' . (is_array(json_decode($apiRequest, TRUE)) ? wikiArrayToList(json_decode($apiRequest, TRUE)) : '<h5 style="color:red;">Request did not return a json formatted string! Use format=json in your request url or post array to get a json result.</h5><xmp>' . $apiRequest) . '</xmp></div>';
+        break;
+    case 'mmhdata':
+        $output .= '<div style="height:420px;overflow:auto;"><h3>Wiki Tester : Query Example</h3><p>Tested Url : ' . '<a href="' . $apiRequestUrl . '">' . $apiRequestUrl . '</a>' . '</p><p>Tested $_POST values : ' . wikiArrayToList($post, 'POST') . '</p>' . (is_array(json_decode($apiRequest, TRUE)) ? wikiArrayToList(json_decode($apiRequest, TRUE)) : '<h5 style="color:red;">Request did not return a json formatted string! Use format=json in your request url or post array to get a json result.</h5><xmp>' . $apiRequest) . '</xmp></div>';
+        break;
+    default:
+        $output .= '<ul><li><a href="' . $sdmcore->sdmCoreGetRootDirectoryUrl() . '/index.php?page=wikiTester&wikiquerymode=wikidata">Wiki Data Query Test</a></li>';
+        $output .= '<li><a href="' . $sdmcore->sdmCoreGetRootDirectoryUrl() . '/index.php?page=wikiTester&wikiquerymode=mmhdata">MMh Mined Wiki Data Query Reults</a></li></ul>';
+        break;
+}
+
 $sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $output, array('incpages' => array('wikiTester')), FALSE);
 
