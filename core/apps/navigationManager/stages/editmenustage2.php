bcd510250431571c8a36a65fb3b58c38041a8718
sevidmusic <sevi@moviemeetinghouse.com>
2015-05-18 01:15:54 -0400

    Navigation Manager: Added links to edit menu items forms to editmenustage2.php

--- core/apps/navigationManager/stages/editmenustage2.php
+++ core/apps/navigationManager/stages/editmenustage2.php
@@ -99,4 +99,10 @@ $editMenuSelectMenuForm->form_elements = array(
 );
 $editMenuSelectMenuForm->sdmFormBuildForm($sdmcore->sdmCoreGetRootDirectoryUrl());
 $output = '<div id="originalMenuPreview" style="padding:20px;border:3px dashed #777777;border-radius:7px;"><h4>Menu Preview</h4>' . $menuHtml . '</div>';
-$sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $output . $editMenuSelectMenuForm->sdmFormGetForm(), array('incpages' => array('navigationManagerEditMenuStage2')));
+$output .= '<div><h3>Menu Items</h3><ul>';
+foreach ($menu->menuItems as $menuItem) {
+    $arguments = array('page=navigationManagerEditMenuStage3_editmenuitem', 'menuItemId=' . $menuItem->menuItemId, 'linkedBy=navigationManager_editmenustage2_editMenuItemLink');
+    $output .= '<li>' . $menuItem->menuItemDisplayName . ' (<a href = "' . $sdmcore->sdmCoreGetRootDirectoryUrl() . '/index.php?' . str_replace('%26', '&', str_replace('%3D', '=', urlencode(implode('&', $arguments)))) . '">edit</a> | delete)</li>';
+}
+$output .= '</ul></div>';
+$sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $output . '<h3>Menu Settings</h3>' . $editMenuSelectMenuForm->sdmFormGetForm(), array('incpages' => array('navigationManagerEditMenuStage2')));
