739b521c93ed93079a8c97c184b11f60eb8ddbef
sevidmusic <sevi@moviemeetinghouse.com>
2015-06-05 07:03:26 -0400

    Themes: Added new themes sdm2 and sdm3.Removed theme_images folder from themes sdm, sdm2, and sdm3.

---
+++ themes/sdm3/page.php
@@ -0,0 +1,7 @@
+<div id="lockedwrapper">
+    <div id='main_content'>
+        <?php
+        echo SdmAssembler::sdmAssemblerGetContentHtml('main_content', $sdmassembler_themeContentObject);
+        ?>
+    </div>
+</div>
