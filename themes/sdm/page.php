7ea9b3a280b34b1e4836a6ad3bd8b1a9f3054a0b
sevidmusic <sevi@moviemeetinghouse.com>
2015-06-24 07:48:40 -0400

    Themes: Removed theme sdm.

--- themes/sdm/page.php
+++
@@ -1,7 +0,0 @@
-<div id="lockedwrapper">
-    <div id='main_content'>
-        <?php
-        echo SdmAssembler::sdmAssemblerGetContentHtml('main_content', $sdmassembler_themeContentObject);
-        ?>
-    </div>
-</div>
