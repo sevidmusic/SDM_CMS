4f699ea98aa0e6090c937f77016945a69fee8eec
sevidmusic <sevi@moviemeetinghouse.com>
2015-07-02 01:33:15 -0400

    SdmGatekeeper: Added method sessionConfigInfo() which utilizes sdmCoreSdmReadArray() to show info about the current configuration of PHP sessions.

--- core/includes/SdmGatekeeper.php
+++ core/includes/SdmGatekeeper.php
@@ -9,8 +9,10 @@
  * filters, and random data generators that can be used to create unique ids,
  * security tokens, security keys, and cipher keys.
  */
-class SdmGatekeeper implements SessionHandlerInterface {
+class SdmGatekeeper extends SdmCore implements SessionHandlerInterface {
 
+    /** Custom Session Handler Methods */
+    // NOTE: Theses methods overwrite the default session handlers provided by PHP //
     public function read($session_id) {
         return;
     }
@@ -33,6 +35,28 @@ class SdmGatekeeper implements SessionHandlerInterface {
 
     public function gc($maxlifetime) {
         return;
+    }
+
+    /** Session Info Methods */
+
+    /**
+     * Utilizes sdmCoreSdmReadArray() to show info
+     * about the current configuration of PHP sessions.
+     * This method is useful for debugging and development.
+     * @return boolean Returns TRUE.
+     */
+    public function sessionConfigInfo() {
+        $sessionConfigInfo = array(
+            'session_module_name' => session_module_name(),
+            'session_id' => (session_id() === '' ? 'There is no session id at the moment which means there is no active session' : session_id()),
+            'session_name' => session_name(),
+            'session_save_path' => session_save_path(),
+            'session_get_cookie_params' => session_get_cookie_params(),
+            'session_cache_limiter' => session_cache_limiter(),
+            '$_SESSION' => (!empty($_SESSION) === TRUE ? $_SESSION : 'No current $_SESSION data'),
+        );
+        $this->sdmCoreSdmReadArray($sessionConfigInfo);
+        return TRUE;
     }
 
 }
