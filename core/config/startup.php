9b0f43b2c9ead1b475989fdc203d40ebdbbfe608
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-10 20:23:24 -0400

    startup.php: Cleaned up startup.php code.

--- core/config/startup.php
+++ core/config/startup.php
@@ -10,7 +10,7 @@ function __autoload($classes) {
 }
 
 // temp | configure session max lifetime | move this somehwere more reasonable
-ini_set('session.gc_maxlifetime', 5); //10 seconds  | sec * min * hour
+ini_set('session.gc_maxlifetime', 5); // set in seconds
 ini_set('session.gc_probability', 100); // chance that GC will occur
 ini_set('session.gc_divisor', 100); // probability divisor, if gc_propbability is 50 and gc_divisor is 100 then there is a 50% chance of GC (i.e. 50/100)
 // dev //
@@ -26,9 +26,8 @@ $sdmGatekeeper->sessionStart();
 $_SESSION['referer_token'] = $sdmGatekeeper->sdmKind($sdmcore->sdmCoreGetRootDirectoryUrl());
 // store decoded refer_token in $_SESSION, if it is not === to the site root url then this request is not from our site
 $_SESSION['site_root_url'] = ($sdmGatekeeper->sdmNice($_SESSION['referer_token']) === $sdmcore->sdmCoreGetRootDirectoryUrl() ? $sdmGatekeeper->sdmNice($_SESSION['referer_token']) : 'invalid_referer');
-// store session config info in $_SESSION while in DEV. !REMOVE ONCE OUT OF DEV FOR SECURITY!!!
-$_SESSION['config'] = $sdmGatekeeper->sessionConfigInfo(TRUE);
-//$sdmcore->sdmCoreSdmReadArray($_SESSION);
+// output session data while in dev
+$sdmGatekeeper->sessionConfigInfo();
 // initialize assembler
 $sdmassembler = SdmAssembler::sdmAssemblerInitializeAssembler();
 // load and assemble content | this var is used excluisively by the current themes page.php
