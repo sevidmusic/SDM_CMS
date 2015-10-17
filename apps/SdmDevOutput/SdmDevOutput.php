1d7d730725855382ea9c6af7fbfbb0536eb46356
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-17 14:40:59 -0400

    SdmDevOutput: Added brackets to if statement for readability.

--- apps/SdmDevOutput/SdmDevOutput.php
+++ apps/SdmDevOutput/SdmDevOutput.php
@@ -3,8 +3,9 @@
 function sdmStrSlice($string, $start, $end) {
     $string = " " . $string;
     $ini = strpos($string, $start);
-    if ($ini == 0)
+    if ($ini == 0) {
         return "";
+    }
     $ini += strlen($start);
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
