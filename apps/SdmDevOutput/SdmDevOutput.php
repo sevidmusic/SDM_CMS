c260670b265d897bee3c40e3ea6ac0c4d0cd5736
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-17 11:14:11 -0400

    SdmDevOutput: Modified app to test new sdmStrSlice() function. This function reads a substring between two strings in a string. For instance, sdmStrSlice(some example string, some, string) would return example. This function will be moved into core as a method. It was created to give the SdmGatekeeper() the ability to read multiple parameters from .gk files, howvever it will be available to developers via core for their apps and themes as well; contentManager: Set .gk roles to root;

--- apps/SdmDevOutput/SdmDevOutput.php
+++ apps/SdmDevOutput/SdmDevOutput.php
@@ -1,28 +1,20 @@
 <?php
 
-echo '<span style="color:#ffffff;">Session Id: ' . session_id() . '</span>';
-/** Log of what is being tested */
-$tests = array(
-    'SdmGateKeeper()' . rand(1000, 9999) => 'Checking to see if sessions are working properly',
-    'SdmGateKeeper()' . rand(100, 999) => 'Testing that sessionConfigInfo() method is working',
-);
-
-/** TESTS */
-$sdmGatekeeper = new SdmGatekeeper();
-$_SESSION['RequestedPage'] = $sdmcore->SdmCoreDetermineRequestedPage();
-$sdmGatekeeper->sessionConfigInfo(FALSE, TRUE);
-
-
-/** APP OUTPUT */
-$devOutput = '<h2>Dev Output</h2><p>This app can be used to test, debug, and experiment with snippets of PHP code.</p>';
-// Display current tests logged in $tests array
-$devOutput .= '<p>Current Tests</p><table style="width:100%;color:aqua;border:3px solid white;">';
-foreach ($tests as $key => $value) {
-    $devOutput .= '<tr>
-                <td style="padding:12px;border:1px solid white;">Testing ' . trim(str_replace(range(0, 9), '', $key)) . '</td>
-                <td style="padding:12px;color:#CCCCCC;border:1px solid white;">' . $value . '</td>
-            </tr>';
+function sdmStrSlice($string, $start, $end) {
+    $string = " " . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0)
+        return "";
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
 }
-$devOutput .= '</table>';
-$sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $devOutput, array('wrapper' => 'main_content', 'incmethod' => 'append', 'incpages' => array('SdmDevOutput')));
 
+$app = 'contentManager';
+$params = file($sdmassembler->sdmCoreGetCoreAppDirectoryPath() . '/' . $app . '/' . $app . '.gk');
+$output = '';
+foreach ($params as $param) {
+    $output .= '<h3>' . substr($param, 0, strpos($param, '=')) . ':</h3>';
+    $output .= '<p>' . sdmStrSlice($param, '=', ';') . '</p>';
+}
+$sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $output);
