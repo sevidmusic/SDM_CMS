c94dab20bd2ecaf35d293a3be419a922a66eeebe
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-27 02:15:13 -0400

    jqueryPlay app: Modified code so alert message is only displayed once.

--- apps/jqueryPlay/js/jqueryPlay.js
+++ apps/jqueryPlay/js/jqueryPlay.js
@@ -1,16 +1,20 @@
+var alerted = localStorage.getItem('alerted') || '';
+if (alerted != 'yes') {
 // first check if jQuery has been loaded
-if (typeof jQuery != 'undefined') {
-    // then check if jQuery UI has been loaded
-    if (typeof jQuery.ui != 'undefined') {
-        $("document").ready(function() {
-            /* VARIABLE INIT */
-            alert('jQuery version "' + $.fn.jquery + '" and jQuery UI version "' + $.ui.version + '" are working on this page.');
-        }); // end of $("document").ready(function()
-    }
-    else { // if jQuery UI is missing, alert user
-        alert("MISSING JS RESOURCE:\n\n jQuery UI failed to load properly so some site features may not be available.\n\nPlease report this to the site admin at:\n\nADMINEMAILADDRESS@EMAILSERVER.COM\n\nor at\n\nLINKTOADMINCONTACT.");
-    }
+    if (typeof jQuery != 'undefined') {
+        // then check if jQuery UI has been loaded
+        if (typeof jQuery.ui != 'undefined') {
+            $("document").ready(function() {
+                /* VARIABLE INIT */
+                alert('jQuery version "' + $.fn.jquery + '" and jQuery UI version "' + $.ui.version + '" are working on this page.');
+            }); // end of $("document").ready(function()
+        }
+        else { // if jQuery UI is missing, alert user
+            alert("MISSING JS RESOURCE:\n\n jQuery UI failed to load properly so some site features may not be available.\n\nPlease report this to the site admin at:\n\nADMINEMAILADDRESS@EMAILSERVER.COM\n\nor at\n\nLINKTOADMINCONTACT.");
+        }
 
-} else { // if jQuery is missing alert user
-    alert("MISSING JS RESOURCE:\n\n jQuery failed to load properly so some site features may not be available.\n\nPlease report this to the site admin at:\n\nADMINEMAILADDRESS@EMAILSERVER.COM\n\nor at\n\nLINKTOADMINCONTACT.");
+    } else { // if jQuery is missing alert user
+        alert("MISSING JS RESOURCE:\n\n jQuery failed to load properly so some site features may not be available.\n\nPlease report this to the site admin at:\n\nADMINEMAILADDRESS@EMAILSERVER.COM\n\nor at\n\nLINKTOADMINCONTACT.");
+    }
+    localStorage.setItem('alerted', 'yes');
 }
