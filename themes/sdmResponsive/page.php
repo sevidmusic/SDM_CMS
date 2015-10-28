3f9ff45388ad959ae60175d1a15c3e6b8df3beb1
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-28 14:09:21 -0400

    Theme sdmResponsive: Updated theme.

--- themes/sdmResponsive/page.php
+++ themes/sdmResponsive/page.php
@@ -1,25 +1,34 @@
+<!-- row 1 -->
 <div class="row">
-    <div id="top-menu"class="col-12">
+    <div id="top-menu"class="col-12 border-1">
         <?php
         echo SdmAssembler::sdmAssemblerGetContentHtml('top-menu', $sdmassembler_themeContentObject);
         ?>
     </div>
 </div>
+<!-- row 2 -->
 <div class="row">
-    <div id="side-menu" class="col-3">
-        <?php
-        echo SdmAssembler::sdmAssemblerGetContentHtml('side-menu', $sdmassembler_themeContentObject);
-        ?>    </div>
-    <div id="locked-spacer" class="col-1 spacer"></div>
-    <div id="main_content"class="col-8">
+    <?php
+    $sideBarExists = (SdmAssembler::sdmAssemblerGetContentHtml('side-menu', $sdmassembler_themeContentObject) === '<!-- side-menu placeholder -->' ? FALSE : TRUE);
+    if ($sideBarExists === TRUE) {
+        ?>
+        <div id="side-menu" class="col-3 border-2 gradi-bg">
+            <?php
+            echo SdmAssembler::sdmAssemblerGetContentHtml('side-menu', $sdmassembler_themeContentObject);
+            ?>    </div>
+        <div id="locked-spacer" class="col-1 spacer"></div>
+    <?php } ?>
+    <div id="main_content"class="<?php echo ($sideBarExists === TRUE ? 'col-8' : 'col-12'); ?> border-2 gradi-bg">
         <?php
         echo SdmAssembler::sdmAssemblerGetContentHtml('main_content', $sdmassembler_themeContentObject);
         ?>
     </div>
-    <div class="row">
-        <div id="footer"class="col-12">
-            <?php
-            echo SdmAssembler::sdmAssemblerGetContentHtml('footer', $sdmassembler_themeContentObject);
-            ?>
-        </div>
+</div>
+<!-- row 3 -->
+<div class="row">
+    <div id="footer"class="col-12">
+        <?php
+        echo SdmAssembler::sdmAssemblerGetContentHtml('footer', $sdmassembler_themeContentObject);
+        ?>
     </div>
+</div>
