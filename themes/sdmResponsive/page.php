8a3b0b4dd2c8ee4f9c6ae682a1859c7e49a786fd
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-29 13:55:07 -0400

    Theme sdmResponsive: Added col-m classes to appropriate elements in page.php

--- themes/sdmResponsive/page.php
+++ themes/sdmResponsive/page.php
@@ -1,6 +1,6 @@
 <!-- row 1 -->
 <div class="row">
-    <div id="top-menu"class="col-12 border-1">
+    <div id="top-menu"class="col-12 col-m-12 border-1">
         <?php
         echo SdmAssembler::sdmAssemblerGetContentHtml('top-menu', $sdmassembler_themeContentObject);
         ?>
@@ -12,13 +12,13 @@
     $sideBarExists = (SdmAssembler::sdmAssemblerGetContentHtml('side-menu', $sdmassembler_themeContentObject) === '<!-- side-menu placeholder -->' ? FALSE : TRUE);
     if ($sideBarExists === TRUE) {
         ?>
-        <div id="side-menu" class="col-3 rounded">
+        <div id="side-menu" class="col-3 col-m-3 rounded">
             <?php
             echo SdmAssembler::sdmAssemblerGetContentHtml('side-menu', $sdmassembler_themeContentObject);
             ?>    </div>
-        <div id="locked-spacer" class="col-1 spacer"></div>
+        <div id="locked-spacer" class="col-1 col-m-1 spacer"></div>
     <?php } ?>
-    <div id="main_content"class="<?php echo ($sideBarExists === TRUE ? 'col-8' : 'col-12'); ?> rounded">
+    <div id="main_content"class="<?php echo ($sideBarExists === TRUE ? 'col-8 col-m-8' : 'col-12 col-m-12'); ?> rounded">
         <?php
         echo SdmAssembler::sdmAssemblerGetContentHtml('main_content', $sdmassembler_themeContentObject);
         ?>
@@ -26,7 +26,7 @@
 </div>
 <!-- row 3 -->
 <div class="row">
-    <div id="footer"class="col-12">
+    <div id="footer"class="col-12 col-m-12">
         <?php
         echo SdmAssembler::sdmAssemblerGetContentHtml('footer', $sdmassembler_themeContentObject);
         ?>
