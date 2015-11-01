9f89b5f2d85c6007008cc4d501fccdcb7067725c
sevidmusic <sevi@moviemeetinghouse.com>
2015-10-31 22:12:29 -0400

    SdmContentGenerator user app: Now generating content more effeciently, added new functions, randStr and randPara, that should be converted into mehtods in either the SdmCms or SdmCore class

--- apps/SdmContentGenerator/SdmContentGenerator.php
+++ apps/SdmContentGenerator/SdmContentGenerator.php
@@ -3,18 +3,54 @@
 $options = array('incpages' => array('SdmContentGenerator'));
 $output = '<h2>Sdm Content Generator</h2><p>Generates dev content for the SDM CMS</p>';
 $cm = new SdmCms();
-$genPages = array();
-$limit = 500;
-for ($i = 0; $i < $limit; $i++) {
-    $genPages['gen_page_' . $i . '_' . rand(10000, 99999)] = rand(100000000000, 999999999999);
+
+function randString($mode, $limit) {
+    $strLen = rand(1, $limit);
+    $string = '';
+    switch ($mode) {
+        case 'alphaNum':
+            $charArr = str_split('abcdefgabcdefghijklmnopqrstuvwxyz0123456789');
+            for ($i = 0; $i < $strLen; $i++) {
+                $string .= $charArr[array_rand($charArr)];
+            }
+            break;
+        case 'alpha':
+            $charArr = str_split('abcdefgabcdefghijklmnopqrstuvwxyz');
+            for ($i = 0; $i < $strLen; $i++) {
+                $string .= $charArr[array_rand($charArr)];
+            }
+            break;
+        case 'num':
+            $charArr = str_split('0123456789');
+            for ($i = 0; $i < $strLen; $i++) {
+                $string .= $charArr[array_rand($charArr)];
+            }
+            break;
+    }
+    return $string;
 }
-$status = array();
+
+function randPara($lineLimit, $wordPerLine = 12, $wordLen = 12, $mode = NULL) {
+    $para = '';
+    $modes = array('alphaNum', 'alpha', 'num');
+    // limit by $lineLimit
+    for ($cycle = 0; $cycle < $lineLimit; $cycle++) {
+        $para .= '<p>';
+        // limit by $wordPerLine
+        for ($i = 0; $i < $wordPerLine; $i++) {
+            // create line
+            $para .= randString(($mode != NULL ? $mode : $modes[array_rand($modes)]), $wordLen) . ' ';
+        }
+        // end line
+        $para .= '</p>';
+    }
+    return $para;
+}
+
 if ($sdmassembler->sdmCoreDetermineRequestedPage() === 'SdmContentGenerator') {
-    foreach ($genPages as $id => $html) {
-        $status[] = $cm->sdmCmsUpdateContent($id, 'main_content', $html);
+    $pageLimit = 5;
+    for ($index = 0; $index < $pageLimit; $index++) {
+        $cm->sdmCmsUpdateContent(randString('alpha', 8), 'main_content', randPara(12, 100, 9, 'alpha'));
     }
 }
-$output .= '<p>';
-$output .= (in_array(FALSE, $status, TRUE) ? 'Could not generate pages' : $limit . ' pages were generated.');
-$output .= '</p>';
 $sdmassembler->sdmAssemblerIncorporateAppOutput($sdmassembler_dataObject, $output, $options);
