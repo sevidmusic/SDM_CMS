f7bdf2e06f1cfb830c8649210b15f16f0142f0a9
sevidmusic <sevi@moviemeetinghouse.com>
2015-11-10 23:38:17 -0500

    gitTools user app: Removied closing PHP tag from gitTools.php

--- apps/gitTools/gitTools.php
+++ apps/gitTools/gitTools.php
@@ -18,4 +18,3 @@ $output = "<div style='clear:both;color: #66CCCC; background: #000000; border-ra
 $sdmassembler->sdmAssemblerIncorporateAppOutput($sdmAssemblerDataObject, $output, array('incmethod' => 'prepend'));
 
 $sdmassembler->sdmAssemblerIncorporateAppOutput($sdmAssemblerDataObject, '<h3>Git Tools</h3><p>This app provides tools for working with git. At the moment the only tool is a <i>branch viewer</i> that show the current git branch at the top of the main_content menu wrapper on every page.</p><p>Further development of this app will occur in the future, and this app will have a variety of tools for using git with your SDM CMS site.</p>', array('incpages' => array('gitTools'), 'incmethod' => 'append'));
-?>
