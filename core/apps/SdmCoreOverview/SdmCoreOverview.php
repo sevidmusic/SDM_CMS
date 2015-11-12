2b1be0d6ae77a8dec777e598f896b2a36d16fa24
sevidmusic <sevi@moviemeetinghouse.com>
2015-11-12 14:34:57 -0500

    SdmCoreOverview core app: Now using output buffering to capture the results of var_dump($dataObject) to a string so we can display the var_dump() output within the structure of the current theme via the SdmAssembler()::sdmAssemblerIncorporateAppOutput() method provided by the SdmAssembler();

--- core/apps/SdmCoreOverview/SdmCoreOverview.php
+++ core/apps/SdmCoreOverview/SdmCoreOverview.php
@@ -7,7 +7,9 @@ $output = '<h2>Overview of current state the CORE</h2><p>The SDM Core Overview a
 // load the core overview file
 $dataObject = $sdmassembler->sdmCoreGetDataObject();
 if ($sdmassembler->sdmCoreDetermineRequestedPage() === 'SdmCoreOverview') {
+    ob_start();
     var_dump($dataObject);
+    $output .= ob_get_clean();
 }
 // incorporate core overview
 $sdmassembler->sdmAssemblerIncorporateAppOutput($sdmAssemblerDataObject, $output, $options);
