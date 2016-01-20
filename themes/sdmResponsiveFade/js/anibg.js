6d4eab161393cf7e5bd770ccc5b1635cb895384e
sevidmusic <sevidmusic@gmail.com>
2016-01-20 06:48:03 -0500

    sdmResponsiveFade: Finished developing anibg.js. Now anibg.js creates an array of random colors and animates the page background color through the random colors in the array. Very cool effect, this theme may end up being the default theme as it shows the SDM CMS in action as it makes use of the user apps jQuer and jQuery Ui, and therefore javascript and jquery and jquery ui. Also demonstrates how themes and apps can serve stylesheets and scripts to the page via .as files. Finally the theme is responsive in design which demonstrates that themes can be created for the SDM CMS that utilize modern design techniques.

--- themes/sdmResponsiveFade/js/anibg.js
+++ themes/sdmResponsiveFade/js/anibg.js
@@ -3,26 +3,39 @@ $(document).ready(function () {
     var imageUrl = "http://localhost:8888/sdm_cms/themes/sdmResponsiveFade/imgs/sdm-cms-logo.png";
 
     /* Insert background image */
-    $("html body").css('background-image', 'url(' + imageUrl + ')');
+    //$("html body").css('background-image', 'url(' + imageUrl + ')');
 
     /**
-     *
-     * @param target
-     * @param color
-     * @param aniTime
+     * Animates the background color of a specified element.
+     * @param target The target element.
+     * @param colors An array of colors to animate through.
+     * @param aniTime The time in mili-seconds it takes to animate between each color.
      */
     function aniBg(target, colors, aniTime, index) {
         var limit = colors.length;
         var newIndex = ((index + 1) < limit) ? index + 1 : 0; // reset index if {index + 1} !< limit;
-        console.log('limit: ' + limit + ' | index: ' + index + ' | newIndex:' + newIndex);
+        // console.log('limit: ' + limit + ' | index: ' + index + ' | newIndex:' + newIndex);
         $(target).animate({backgroundColor: colors[index]}, aniTime, function () {
-
-            $(target).animate({backgroundColor: colors[newIndex]}, aniTime, function () {
+            // console.log('color: ' + colors[index]);
+            $(target).animate({backgroundColor: colors[newIndex]}, aniTime - (aniTime / 2), function () {
+                // console.log(colors[newIndex]);
                 aniBg(target, colors, aniTime, newIndex);
             });
 
         });
     }
-    /* Animate Background Color */
-    aniBg("html body",['#ffffff', '#00FF77'], 3420, 0);
+
+    colorsArr = ['#ffcc33', '#000000'];
+    var alpha = '0 1 2 3 4 5 6 7 8 9 A B C D E F';
+    var alphaArr = alpha.split(" ");
+    /* Create Random Array of Random Colors */
+    for(var i = 0; i < 100; i++) {
+        var randCharOne = alphaArr[Math.floor(Math.random() * alphaArr.length)];
+        var randCharTwo = alphaArr[Math.floor(Math.random() * alphaArr.length)];
+        var randCharThree = alphaArr[Math.floor(Math.random() * alphaArr.length)];
+        var newColor = '#' + randCharOne + randCharOne + randCharTwo + randCharTwo + randCharThree + randCharThree;
+        colorsArr.push(newColor);
+        // console.log('randCharOne: ' + randCharOne + ' | randCharTwo: ' + randCharTwo + ' | randCharThree: ' + randCharThree + ' | pushed color:' + newColor);
+    }
+    aniBg("html body", colorsArr, 1420, 0);
 });
