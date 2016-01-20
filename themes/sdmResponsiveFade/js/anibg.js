45626b793b947f9793653853a725865d276cffdd
sevidmusic <sevidmusic@gmail.com>
2016-01-20 06:57:52 -0500

    Theme sdmResponsiveFade: Removed hard coded colors from colors array in anibg.js;

--- themes/sdmResponsiveFade/js/anibg.js
+++ themes/sdmResponsiveFade/js/anibg.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
         });
     }
 
-    colorsArr = ['#ffcc33', '#000000'];
+    colorsArr = [];
     var alpha = '0 1 2 3 4 5 6 7 8 9 A B C D E F';
     var alphaArr = alpha.split(" ");
     /* Create Random Array of Random Colors */
