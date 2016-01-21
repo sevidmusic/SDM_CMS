54d4bd1ab22cc46f2104ddf81895b08d29c72390
sevidmusic <sevidmusic@gmail.com>
2016-01-21 18:25:47 -0500

    Various Components: Removed closing php tag in files that provided it. The SDM CMS encourages developers to omit the closing PHP tag from their apps and thems so it's components needs to abide by the same standard.

--- core/apps/navigationManager/stages/deletemenustage2.php
+++ core/apps/navigationManager/stages/deletemenustage2.php
@@ -1,4 +1,3 @@
 <?php
 
 $sdmassembler->sdmAssemblerIncorporateAppOutput('<h3>Deleted Menu:</h3>' . $sdmassembler->sdmNmsDeleteMenu(SdmForm::sdmFormGetSubmittedFormValue('menuId')), array('incpages' => array('navigationManagerDeleteMenuStage2')));
-?>
--- core/includes/SdmNms.php
+++ core/includes/SdmNms.php
@@ -360,5 +360,3 @@ class SdmNms extends SdmGatekeeper
     }
 
 }
-
-?>
