f0334a6dc82af9b30d05b9a2a770a0ebccf6cb12
sevidmusic <sevidmusic@gmail.com>
2016-02-10 14:10:31 -0500

    contentManager core app: Refactored contentManagerAdministerAppsFormSubmission.php to use switch instead of if else.

--- core/apps/contentManager/handlers/contentManagerAdministerAppsFormSubmission.php
+++ core/apps/contentManager/handlers/contentManagerAdministerAppsFormSubmission.php
@@ -13,44 +13,45 @@ $on = null;
 /* Tracks disabled apps. */
 $off = array();
 
-/* Determine currently enabled apps. */
+/* Determine currently available apps. */
 $availableApps = $sdmcms->sdmCmsDetermineAvailableApps();
 
 /* Check if form was submitted successfully. */
-if (SdmForm::sdmFormGetSubmittedFormValue('content_manager_form_submitted') === 'content_manager_form_submitted') {
-    /* Loop through available apps updating state if necessary. */
-    foreach ($availableApps as $appname => $app) {
-        $newAppState = SdmForm::sdmFormGetSubmittedFormValue($app);
-        $sdmcms->sdmCmsSwitchAppState($app, $newAppState);
-        $on = $sdmcms->sdmCoreDetermineEnabledApps();
-        if ($newAppState !== 'on') {
-            $off[0] = '<span style="color:red;">Reworking the display of disabled apps.!</span>';
+switch(SdmForm::sdmFormGetSubmittedFormValue('content_manager_form_submitted')){
+    case 'content_manager_form_submitted':
+        /* Loop through available apps updating state if necessary. */
+        foreach ($availableApps as $appname => $app) {
+            $newAppState = SdmForm::sdmFormGetSubmittedFormValue($app);
+            $sdmcms->sdmCmsSwitchAppState($app, $newAppState);
+            $on = $sdmcms->sdmCoreDetermineEnabledApps();
+            if ($newAppState !== 'on') {
+                $off[0] = '<span style="color:red;">Reworking the display of disabled apps.!</span>';
+            }
         }
-    }
-    $output .= '<h4>The following apps are enabled:</h4><ul>';
-    foreach ($on as $enabledApp) {
-        $output .= '<li style="color:#00C957;">' . $enabledApp . '</li>';
-    }
-    $output .= '</ul>';
-    $output .= '<h4>The following apps are disabled:</h4><ul>';
-    foreach ($off as $disabledApp) {
-        $output .= '<li style="color:#00BFFF;">' . $disabledApp . '</li>';
-    }
-    $output .= '</ul>';
-    //$sdmcms->sdmCmsSwitchAppState('contentManager', 'off');
-    $output .= '
-                    <!-- contentManager div -->
-                    <div id"contentManager">
-                        <p>Form has been submitted.</p>
-                    </div>
-                    <!-- close contentManager div -->';
-} // form submitted but error occured
-else {
-    $output .= '
+        $output .= '<h4>The following apps are enabled:</h4><ul>';
+        foreach ($on as $enabledApp) {
+            $output .= '<li style="color:#00C957;">' . $enabledApp . '</li>';
+        }
+        $output .= '</ul>';
+        $output .= '<h4>The following apps are disabled:</h4><ul>';
+        foreach ($off as $disabledApp) {
+            $output .= '<li style="color:#00BFFF;">' . $disabledApp . '</li>';
+        }
+        $output .= '</ul>';
+        $output .= '<!-- contentManager div -->
+                <div id"contentManager">
+                  <p>Form has been submitted.</p>
+                </div>
+                <!-- close contentManager div -->';
+        break;
+    default:
+        $output .= '
                 <div id="contentManager">
-                    <p>And error occured and the form could not be submitted</p>
+                    <p>And error occurred and the form could not be submitted</p>
                 </div>
                 ';
+        break;
 }
 
+/* Incorporate app output. */
 $sdmassembler->sdmAssemblerIncorporateAppOutput($output, $options);
