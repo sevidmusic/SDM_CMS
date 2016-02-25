c9912a1c9fef43bdd994588f95d07d467544fa3c
sevidmusic <sevidmusic@gmail.com>
2016-02-25 16:16:26 -0500

    jQuery user app: Added jQuery effects to jQuery app page. Added new file jQueryAnimations.js which provides the jQuery code for the new effects.

--- apps/jQuery/jquery.php
+++ apps/jQuery/jquery.php
@@ -7,19 +7,13 @@
  *
  */
 
-$output = '
+$output = '<div id="jQueryApp">
 <h2>jQuery</h2></h2>
 <p>Provides the jQuery library.</p>
 <p id="jQueryStatus"><!-- jQueryStatus.js output will end up here --></p>
-';
+</div>';
 $options = array(
     'incpages' => array('jQuery'),
 );
 
 $sdmassembler->sdmAssemblerIncorporateAppOutput($output, $options);
-
-$sdmassembler->sdmAssemblerIncorporateAppOutput('
-<p>Note: The jQuery app must be enabled before any apps or themes dependent on
-jQuery can be used. If an app or theme that depends on jQuery was enabled before
-the jQuery app then you will have to disable the dependent app, make sure jQuery is enabled,
-and then re-enable the dependent app</p>', ['incpages' => ['jQuery'], 'incmethod' => 'append']);
---
+++ apps/jQuery/js/jQueryAppAnimations.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sevidmusic on 2/25/16.
+ */
+
+$(document).ready(function () {
+    $("#jQueryApp").css("opacity", "0");
+    $("#jQueryApp").animate({opacity: "1", color: "#75FC62"}, 1200);
+    $("#jQueryApp").animate({opacity: "1", color: "#1CB1F4"}, 1020);
+    $("#jQueryApp").animate({opacity: "1", color: "inherit"}, 920);
+
+});
