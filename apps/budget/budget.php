b250274769c0ba85efedb31650642c94e793dc75
sevidmusic <sevidmusic@gmail.com>
2016-03-19 07:34:19 -0400

    Added new user app, Budget, whose code can be modified to output a budget based on an available balance, and the expected expensses.

---
+++ apps/budget/budget.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Hello World user app: This app demonstrates a simple Sdm Cms user app.
+ */
+
+/* Configure incorporation options. */
+$options = array(
+    'wrapper' => 'main_content',
+    'incmethod' => 'overwrite',
+    'incpages' => array('budget'),
+    'ignorepages' => array(),
+    'roles' => array('all'),
+);
+
+$availableBalance = 372.72;
+$expenses = array(
+    'Car Insurance' => 170.23,
+    'Php Storm' => 8.9,
+    'Gas' => 85,
+    'Tolls' => 7.5,
+    'Cigarettes' => (10.25 * 4), // price of 1 pack times number of packs
+    'Weed' => 50,
+    'Laundry' => 7.5,
+
+);
+$totalExpenses = array_sum($expenses);
+$availableAfterExpenses = $availableBalance - $totalExpenses;
+
+/* Create some output. */
+$output = '<div id="helloWorld"><h4 class="center">Budget ' . date('F d, Y') . '</h4>';
+
+/* Funds Table */
+$output .= '<table class="rounded">
+            <thead>Summary of available balance and expenses:</thead>
+              <tr class="positive">
+                <td>Available Balance:</td>
+                <td>$' . $availableBalance . '</td>
+              </tr>
+              <tr class="negative">
+                <td>Total Expenses</td>
+                <td>$' . $totalExpenses . '</td>
+              </tr>
+              <tr class="' . ($availableAfterExpenses > 0 ? 'positive' : 'negative') . '">
+                <td>Available Balance After Expenses</td>
+                <td>$' . $availableAfterExpenses . '</td>
+              </tr>
+            </table>';
+
+/* Expenses Table */
+$output .= '<table class="rounded">';
+$output .= '<thead>Expenses:</thead>';
+$color = true;
+foreach ($expenses as $expense => $amount) {
+    $bgColor = ($color === true ? '#000000' : '#777777');
+    $output .= '<tr style="background: ' . $bgColor . '"><td>' . $expense . '</td><td>$' . $amount . '</td></tr>';
+    $color = ($color === true ? false : true);
+}
+$output .= '</table></div>';
+
+/* Incorporate output. */
+$sdmassembler->sdmAssemblerIncorporateAppOutput($output, $options);
