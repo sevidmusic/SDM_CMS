89a73a22ef71a189842391fe6cdaf8a4a28b8ea9
sevidmusic <sevidmusic@gmail.com>
2016-03-24 14:33:50 -0400

    Budget user app: Setup categorized expenses. @todo: need to actually do the calculations...

--- apps/budget/budget.php
+++ apps/budget/budget.php
@@ -21,6 +21,43 @@ $expenses = array(
     'Toll' => 1.5,
 );
 
+$categorizedExpenses = array(
+    'Category One' => array(
+        'expense one' => 20.73,
+        'expense two' => 4.20,
+        'expense three' => 3.38,
+    ),
+    'Category Two' => array(
+        'expense one' => 150.2,
+        'expense two' => 5.00,
+        'expense three' => 5.73,
+        'expense four' => 20.23,
+        'expense five' => .73,
+    ),
+    'Category Three' => array(
+        'expense one' => .25,
+        'expense two' => 1.73,
+        'expense three' => 2.03,
+        'expense four' => 12.33,
+        'expense five' => 4.73,
+    ),
+    'Category Four' => array(
+        'expense one' => 1.98,
+        'expense two' => 3.37,
+    ),
+    'Category Five' => array(
+        'expense one' => 120.08,
+        'expense two' => 230.23,
+        'expense three' => 23.04,
+    ),
+    'Category Six' => array(
+        'expense one' => 98.73,
+    ),
+    'Category Seven' => array(
+        'expense one' => 73.21,
+    ),
+);
+
 $totalExpenses = array_sum($expenses);
 
 /* Available Balance Form */
@@ -101,7 +138,23 @@ foreach ($expenses as $expense => $amount) {
 }
 $output .= '</table></div>';
 
-$output .= 'Total Bugdeted for groceries = $' . ($expenses['Cheese'] + $expenses['Cat Food (wet)'] + $expenses['Milk'] + $expenses['Tortillas']);
+/* Categorized Expenses Table */
+$output .= '<table class="rounded">';
+$output .= '<thead>Categorized Expenses:</thead>';
+$color = true;
+foreach ($categorizedExpenses as $category => $categoryExpenses) {
+    $output .= '<tr><th style="text-align: left;">' . $category . '</th></tr>';
+    foreach ($categoryExpenses as $expenses => $amount) {
+        $bgColor = ($color === true ? '#000000' : '#777777');
+        $output .= '<tr style="background: ' . $bgColor . ';"><td>' . $expense . '</td><td>$' . $amount . '</td></tr>';
+        $color = ($color === true ? false : true);
+    }
+    $categoryExpenseTotal = array_sum($categoryExpenses);
+
+    $output .= '<tr class="' . ($categoryExpenseTotal <= 0 ? 'positive' : 'negative') . '"><td style="text-align: left;">' . 'Total Category Expense: ' . $categoryExpenseTotal . '</td></tr>';
+}
+$output .= '</table></div>';
+
 
 /* Incorporate output. */
 $sdmassembler->sdmAssemblerIncorporateAppOutput($output, $options);
