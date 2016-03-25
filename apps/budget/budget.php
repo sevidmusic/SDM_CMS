dc208695cefd8dc77249f3d772c864e805e3ae4e
sevidmusic <sevidmusic@gmail.com>
2016-03-24 22:05:31 -0400

    Budget user app: Setup saveBudget form. Upon submission saveBudget form will keep any previously submitted form values alive so user can continue editing and saving budget without having to re-enter values.

--- apps/budget/budget.php
+++ apps/budget/budget.php
@@ -140,12 +140,50 @@ $balanceOverviewTable = '<table class="rounded">
               </tr>
             </table>';
 
+/* Save budget form. */
+$saveBudgetForm = new SdmForm();
+$saveBudgetForm->method = 'post';
+$saveBudgetForm->formHandler = 'budget';
+$saveBudgetForm->formElements = array(
+    array(
+        'id' => 'budgetCreationTime',
+        'type' => 'hidden',
+        'element' => 'Budget Creation Time',
+        'value' => time(),
+        'place' => '1',
+    ),
+    array(
+        'id' => 'availableCash',
+        'type' => 'hidden',
+        'element' => 'Available Cash',
+        'value' => strval($availableBalanceForm->sdmFormGetSubmittedFormValue('availableCash')),
+        'place' => '2',
+    ),
+    array(
+        'id' => 'availableDebit',
+        'type' => 'hidden',
+        'element' => 'Available Debit',
+        'value' => strval($availableBalanceForm->sdmFormGetSubmittedFormValue('availableDebit')),
+        'place' => '3',
+    ),
+    array(
+        'id' => 'availableCredit',
+        'type' => 'hidden',
+        'element' => 'Available Credit',
+        'value' => strval($availableBalanceForm->sdmFormGetSubmittedFormValue('availableCredit')),
+        'place' => '4',
+    ),
+);
+$saveBudgetForm->submitLabel = 'Save Budget';
+$saveBudgetForm->sdmFormBuildForm();
+
 /* App Output */
 $output = $budgetTitle;
 $output .= $availableBalanceFormHtml;
 $output .= $balanceOverviewTable;
 $output .= $categorizedExpensesTable;
 $output .= $expensesTable;
+$output .= $saveBudgetForm->sdmFormGetForm();
 
 /* Incorporate output. */
 $sdmassembler->sdmAssemblerIncorporateAppOutput($output, $options);
