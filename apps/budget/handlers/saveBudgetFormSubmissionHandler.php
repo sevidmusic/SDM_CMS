87bb21687909dce172e82304ca07842d268be9b7
sevidmusic <sevidmusic@gmail.com>
2016-03-28 20:45:22 -0400

    Budget user app: Saved budget data is now encrypted.

--- apps/budget/handlers/saveBudgetFormSubmissionHandler.php
+++ apps/budget/handlers/saveBudgetFormSubmissionHandler.php
@@ -2,19 +2,28 @@
 
 /* Save form handler */
 if ($saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') !== null) {
-    $budgetJson = json_encode(array(
+    $budgetJson = $sdmassembler->sdmKind(json_encode(array(
         'budgetTitle' => $budgetTitle,
         'budgetID' => $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId'),
         'availableCash' => $saveBudgetForm->sdmFormGetSubmittedFormValue('availableCash'),
         'availableDebit' => $saveBudgetForm->sdmFormGetSubmittedFormValue('availableDebit'),
         'availableCredit' => $saveBudgetForm->sdmFormGetSubmittedFormValue('availableCredit'),
         'categorizedExpenses' => $categorizedExpenses,
-    ));
-    $sdmassembler->sdmCoreSdmReadArray($budgetJson);
+    )));
+
     if (file_put_contents($sdmassembler->sdmCoreGetUserAppDirectoryPath() . '/budget/budgets/' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . '.json', $budgetJson) > 0) {
         $output .= '<div style="color:springgreen;">"' . $budgetTitle . '" was saved to "' . $sdmassembler->sdmCoreGetUserAppDirectoryPath() . '/budget/budgets/' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . '.json"</div>';
     } else {
         error_log('User App "Budget": Attempt to save budget ' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . ' to ' . $sdmassembler->sdmCoreGetUserAppDirectoryPath() . '/budget/budgets/' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . '.json  failed.');
         $output .= '<div style="color:red;">Attempt to save budget with id "' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . '" to following location failed:<br><br>"' . $sdmassembler->sdmCoreGetUserAppDirectoryPath() . '/budget/budgets/' . $saveBudgetForm->sdmFormGetSubmittedFormValue('budgetId') . '.json"</div>';
     }
+
+
+    // DEV CODE //
+    $sdmassembler->sdmCoreSdmReadArray(
+        array(
+            'Budget Data (Encrypted by default for security)' => $budgetJson,
+            'Decrypted Budget Data' => $sdmassembler->sdmNice($budgetJson),
+        )
+    );
 }
