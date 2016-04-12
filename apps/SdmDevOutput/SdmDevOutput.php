ad47b02af2b9cb499b12c6fdf57fc547c81b1536
sevidmusic <sevidmusic@gmail.com>
2016-04-12 14:12:07 -0400

    User app SdmDevOutput: Updated style definitions. Formatted app output for submitted form values display.; SdmForm(): Updated default form values. Added check to insure method passed to $method parameter is supported. @todo: Refactor references to $_GET and $_POST in sdmFormGetSubmittedFormValues() to use PHP's built in filter_ functions to sanitize post, get, and session data accordingly.

--- apps/SdmDevOutput/SdmDevOutput.php
+++ apps/SdmDevOutput/SdmDevOutput.php
@@ -1,59 +1,77 @@
 <?php
 
-/**
- * Sdm Dev Output user app: This app is intended for use by developers. With it php
- * snippets can be tested. Just add the code to test to this file, and incorporate
- * the output via sdmAssemblerIncorporateAppOutput().
- */
+if ($sdmassembler->sdmCoreDetermineRequestedPage() === 'SdmDevOutput') {
+    /**
+     * Sdm Dev Output user app: This app is intended for use by developers. With it php
+     * snippets can be tested. Just add the code to test to this file, and incorporate
+     * the output via sdmAssemblerIncorporateAppOutput().
+     */
 
-$description = '<h1>Sdm Dev Output App</h1><p>This app is intended for use by developers.
+    $description = '<h1>Sdm Dev Output App</h1><p>This app is intended for use by developers.
                     With it php snippets can be tested. Just add the code to test to the
                     SdmDevOutput.php file.</p>';
-$output = '<!-- Sdm Dev Output App Placeholder -->' . $description;
+    $output = '<!-- Sdm Dev Output App Placeholder -->' . $description;
 
-/* Test SdmForm() changes. */
+    /* DEV FORM */
+    $defaultForm = new SdmForm();
+    $defaultForm->formHandler = 'SdmDevOutput';
+    $defaultForm->method = 'post';
+    $defaultForm->sdmFormUseDefaultFormElements();
+    $defaultForm->submitLabel = 'Submit';
+    $defaultForm->sdmFormBuildForm();
+    $defaultFormHtml = $defaultForm->sdmFormGetForm();
 
-/* Build form using default values */
-$defaultForm = new SdmForm();
-$defaultForm->formHandler = 'SdmDevOutput';
-$defaultForm->method = 'post';
-$defaultForm->sdmFormUseDefaultFormElements();
-$defaultForm->submitLabel = 'Submit';
-$defaultForm->sdmFormBuildForm();
+    /* DISPLAY FORM */
+    $output .= $sdmassembler->sdmAssemblerAssembleHtmlElement($defaultFormHtml, array('styles' => array('background: #33CC66', 'border: 3px solid #33CCFF', 'border-radius: 5px', 'padding: 20px')));
+    /* DISPLAY SUBMITTED FORM VALUES */
+    $output .= '<ul>';
+    $submittedValues = $defaultForm->sdmFormGetSubmittedFormValue();
+    if (!empty($submittedValues)) {
+        foreach ($submittedValues as $submittedKey => $submittedValue) {
+            $output .= $sdmassembler->sdmAssemblerAssembleHtmlElement(
+                $sdmassembler->sdmAssemblerAssembleHtmlElement(
+                    $submittedKey,
+                    array(
+                        'styles' => array(
+                            'background: #333333'
+                        ),
+                        'classes' => array(
+                            'sdm-dev-output-padding-all',
+                            'sdm-dev-output-rounded-border',
+                        ),
+                    )
+                ) .
+                $sdmassembler->sdmAssemblerAssembleHtmlElement(
+                    $submittedValue,
+                    array(
+                        'styles' => array(
+                            'background: #000000'
+                        ),
+                        'classes' => array(
+                            'sdm-dev-output-padding-all',
+                            'sdm-dev-output-rounded-border',
+                        ),
+                    )
+                ),
+                array(
+                    'elementType' => 'li',
+                    'styles' => array(
+                        'color: palegreen',
+                        'font-size: .7em',
+                        'background: #33CCFF',
+                        'border: 3px solid #33CC66',
+                        'border-radius: 5px',
+                        'padding: 20px'
+                    )
+                )
+            );
+        }
+    }
 
-$attributes = array(
-    'classes' => explode(' ', 'sdm-dev-output-background sdm-dev-output-border sdm-dev-output-rounded-border sdm-dev-output-float-left sdm-dev-output-height sdm-dev-output-half-wide sdm-dev-output-padding-all sdm-dev-output-margin-bottom'),
-    'elementType' => 'div',
-);
-//$output .= $sdmassembler->sdmCoreSdmReadArrayBuffered($attributes);
-$output .= $sdmassembler->sdmAssemblerAssembleHtmlElement('<h3>Default Form</h3>' . $defaultForm->sdmFormGetForm(), $attributes);
-$output .= '<div class="sdm-dev-output-clear-both"></div>';
+    $output .= '</ul>';
 
-$output .= '<ul>';
-foreach($defaultForm->sdmFormGetSubmittedFormValue() as $submittedKey => $submittedValue) {
-    $output .= $sdmassembler->sdmAssemblerAssembleHtmlElement($submittedKey . ' => ' . $submittedValue, array('elementType' => 'li', 'styles' => array('color: palegreen', 'font-size: .7em')));
+    /* Display app $output */
+    $sdmassembler->sdmAssemblerIncorporateAppOutput($output, ['incpages' => ['SdmDevOutput']]);
+
+
 }
-$output .= '</ul>';
-
-/* variable variables
-$post = $_POST;
-$get = $_GET;
-$session = $_SESSION;
-$method = 'session';
-$data = $$method;
-*/
-$output .= $sdmassembler->sdmCoreSdmReadArrayBuffered($data);
-
-$sdmassembler->sdmAssemblerIncorporateAppOutput($output, ['incpages' => ['SdmDevOutput']]);
-
-
-
-
-
-
-/*
- $submittedDefaultFormValues = $defaultForm->sdmFormGetSubmittedFormValue();
-foreach ($submittedDefaultFormValues as $valKey => $valValue) {
-    $output .= $sdmassembler->sdmAssemblerAssembleHtmlElement($valKey . ': ' . $valValue, array('elementType' => 'p', 'styles' => array('color: red')));
-}
- */
