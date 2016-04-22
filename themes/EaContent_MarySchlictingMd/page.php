5998cd267434d724a671a96e90d9bf9c89f50360
sevidmusic <sevidmusic@gmail.com>
2016-04-22 13:45:04 -0400

    Created new theme EaContent_MarySchilictingMd for new Ea Content client site.

---
+++ themes/EaContent_MarySchlictingMd/page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file defines the basic html structure for the sdmSimple theme.
+ */
+?>
+
+<div id="main_content">
+    <?php
+    echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
+    ?>
+</div>
