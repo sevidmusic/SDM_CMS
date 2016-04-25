09a2328572f7faa549fcbf06fe6206a5ae98bb4a
sevidmusic <sevidmusic@gmail.com>
2016-04-25 12:03:43 -0400

    User app adminBar: Admin panel is no longer hidden by default on admin pages. Made #showAdminBar button a bit bigger.

--- apps/adminBar/js/showHideAdmin.js
+++ apps/adminBar/js/showHideAdmin.js
@@ -6,11 +6,65 @@
 document.cookie = "adminBarDetectJavascriptEnabled=true";
 
 $(document).ready(function () {
-    // Hide admin panel initially
-    $("#adminPanelDisplay").hide();
 
-    // Hide showAdminBar button initially
-    $("#showAdminBar").hide();
+    var $_GET = {};
+
+    document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+        function decode(s) {
+            return decodeURIComponent(s.split("+").join(" "));
+        }
+
+        $_GET[decode(arguments[1])] = decode(arguments[2]);
+    });
+
+    /* Determine requested page. */
+    var requestedPage = $_GET['page'];
+
+    /* Determine what pages to display admin bar and panel on immediately */
+    var displayPages = [
+        'admin',
+        'contentManager',
+        'contentManagerAddContentForm',
+        'contentManagerEditContentForm',
+        'contentManagerSelectPageToEditForm',
+        'contentManagerUpdateContentFormSubmission',
+        'contentManagerSelectPageToDeleteForm',
+        'contentManagerDeletePageSubmission',
+        'contentManagerSelectThemeForm',
+        'contentManagerSelectThemeFormSubmission',
+        'contentManagerAdministerAppsForm',
+        'contentManagerAdministerAppsFormSubmission',
+        'navigationManager',
+        'navigationManagerAddMenuStage1', // select number of menu items
+        'navigationManagerAddMenuStage2', // configure menu items
+        'navigationManagerAddMenuStage3', // configure menu
+        'navigationManagerAddMenuStage4', // add menu
+        'navigationManagerDeleteMenuStage1', // select menu to delete
+        'navigationManagerDeleteMenuStage2', // confirm selected menu should be deleted
+        'navigationManagerDeleteMenuStage3', // delete menu
+        'navigationManagerEditMenuStage1', // select menu to edit
+        'navigationManagerEditMenuStage2', // edit menu settings or select menuItem to edit
+        'navigationManagerEditMenuStage3_submitmenuchanges', // handle edit menu form submission
+        'navigationManagerEditMenuStage3_editmenuitem', // edit menuItem settings
+        'navigationManagerEditMenuStage3_submitmenuitemchanges', // handle edit menu item form submission
+        'navigationManagerEditMenuStage3_confirmdeletemenuitem', // confirm menu item to be deleted
+        'navigationManagerEditMenuStage3_deletemenuitem', // handles deletion of a menu item from a menu through submission of confrim delete menu item form
+        'navigationManagerEditMenuStage3_addmenuitem', // add a menu item to a menu
+        'navigationManagerEditMenuStage3_submitaddmenuitem', // submit added menu item
+        'SdmAuth',
+        'SdmAuthLogin',
+        'SdmCoreOverview',
+        'SdmErrorLog',
+        'Sdm Cms Documentation'
+    ];
+
+    console.log(jQuery.inArray(requestedPage, displayPages));
+
+
+    // Hide admin Bar initially if requestedPage does not match one of the displayPages.
+    if (jQuery.inArray(requestedPage, displayPages) === -1) {
+        $("#adminBarDisplay").hide();
+    }
 
     // Hide admin panel when hideAdminPanel button is clicked
     $("#hideAdminPanel").click(function () {
@@ -42,12 +96,6 @@ $(document).ready(function () {
         $(element).css("opacity", newCssPropertyValues.opacity);
         $(element).css("border-color", newCssPropertyValues.borderColor);
     }
-
-    // enforce initial styles
-    //$(".adminShowHideButton").css("background-color", "#000000;");
-    //$(".adminShowHideButton").css("color", "#ffffff;");
-    //$(".adminShowHideButton").css("opacity", ".5");
-    //$(".adminShowHideButton").css("border-color", "#CCCCCC");
 
     // Unpack original values of css properties that will be modified on rollover
     var initialCssPropertyValues = {
