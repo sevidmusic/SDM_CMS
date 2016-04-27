de76450788e60996db382912591fc97bdfc58de4
sevidmusic <sevidmusic@gmail.com>
2016-04-27 14:51:29 -0400

    Theme EaContent_PaulsMotors: Added new theme file, homepage.php.

---
+++ themes/EaContentPaulsMotors/homepage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ */
+?>
+<!--  -->
+<div id="main_content">
+    <?php
+    echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
+    ?>
+</div>
+<!--  -->
+
+<!--
+  Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
+  This wrapper is meant to hold core and user app output
+  that is not part of the page. For instance, if an app needs to generate
+  a message to the user this core wrapper is a good place to output the message.
+-->
+<div id="Sdm_Cms_Core_Output">
+    <?php
+    echo $sdmassembler->sdmAssemblerGetContentHtml('Sdm_Cms_Core_Output');
+    ?>
+</div>
