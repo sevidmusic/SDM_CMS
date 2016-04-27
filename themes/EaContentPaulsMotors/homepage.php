4ffa36955cca364b34dc217f0802a4cdf71ded57
sevidmusic <sevidmusic@gmail.com>
2016-04-27 15:57:49 -0400

    Theme EaContent_PaulsMotors: Setup page structure of homepage.php, and page.php.

--- themes/EaContentPaulsMotors/homepage.php
+++ themes/EaContentPaulsMotors/homepage.php
@@ -1,15 +1,120 @@
 <?php
 /**
+ * This file defines the structure of the homepage for the EaContent_PaulsMotors theme.
+ *
+ * It was created for Ea Content by Sevi Donnelly Foreman.
+ *
+ * @author: Sevi Donnelly Foreman
+ *
+ * @created: April 27, 2016 at 2:53 p.m.
  *
  */
 ?>
-<!--  -->
-<div id="main_content">
-    <?php
-    echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
-    ?>
+
+<!-- #locked_eac_pm-header-row | .eac_pm-row -->
+<div id="locked_eac_pm-header-row" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-header-logo | .eac_pm-col -->
+    <div id="locked_eac_pm-header-logo" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-logo-text -->
+        <div id="locked_eac_pm-header-logo-text">
+            <h1>Paul's Motors</h1>
+        </div>
+        <!-- End #locked_eac_pm-header-logo-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-logo | .eac_pm-col -->
+
+    <!-- #locked_eac_pm-header-address | .eac_pm-col -->
+    <div id="locked_eac_pm-header-address" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-address-text -->
+        <div id="locked_eac_pm-header-address-text">6 Fairview Ave Poughkeepsie Ny</div>
+        <!-- End #locked_eac_pm-header-address-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-address | .eac_pm-col -->
+
+    <!-- #locked_eac_pm-header-phone-number | .eac_pm-col -->
+    <div id="locked_eac_pm-header-phone-number" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-phone-number-text -->
+        <div id="locked_eac_pm-header-phone-number-text">
+            <h3>845-471-4240</h3>
+        </div>
+        <!-- End #locked_eac_pm-header-phone-number-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-phone-number | .eac_pm-col -->
+
 </div>
-<!--  -->
+<!-- End #locked_eac_pm-header-row | .eac_pm-row -->
+
+<!-- #locked_eac_pm-homepage-row2 | .eac_pm-row -->
+<div id="locked_eac_pm-homepage-row2" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-hours -->
+    <div id="locked_eac_pm-hours">M-F: 8AM - 5:15PM SAT-SUN 8AM - NOON</div>
+    <!-- End #locked_eac_pm-hours -->
+
+    <!-- #locked_eac_pm-main-menu -->
+    <div id="locked_eac_pm-main-menu">
+        <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>Cars</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <!-- End #locked_eac_pm-main-menu -->
+
+</div>
+<!-- End #locked_eac_pm-homepage-row2 | .eac_pm-row -->
+
+
+<!-- #locked_eac_pm-homepage-row3 | .eac_pm-row -->
+<div id="locked_eac_pm-homepage-row3" class="dev eac_pm-row">
+
+    <!-- #eac_pm-homepage-box1 -->
+    <div id="eac_pm-homepage-box1"></div>
+    <!-- End #eac_pm-homepage-box1 -->
+
+</div>
+<!-- End #locked_eac_pm-homepage-row3 | .eac_pm-row -->
+
+
+<!-- #locked_eac_pm-homepage-row4 | .eac_pm-row -->
+<div id="locked_eac_pm-homepage-row4" class="dev eac_pm-row">
+
+    <!-- #eac_pm-homepage-box2 -->
+    <div id="eac_pm-homepage-box2"></div>
+    <!-- End #eac_pm-homepage-box2 -->
+
+</div>
+<!-- End #locked_eac_pm-homepage-row4 | .eac_pm-row -->
+
+
+<!-- #locked_eac_pm-homepage-row5 | .eac_pm-row -->
+<div id="locked_eac_pm-homepage-row5" class="dev eac_pm-row">
+
+    <!-- #eac_pm-homepage-box3 -->
+    <div id="eac_pm-homepage-box3"></div>
+    <!-- End #eac_pm-homepage-box3 -->
+
+</div>
+<!-- End #locked_eac_pm-homepage-row5 | .eac_pm-row -->
+
+
+<!-- #locked_eac_pm-footer-row | .eac_pm-row -->
+<div id="locked_eac_pm-footer-row" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-footer-text -->
+    <div id="locked_eac_pm-footer-text"></div>
+    <!-- End #locked_eac_pm-footer-text -->
+
+</div>
+<!-- End #locked_eac_pm-footer-row | .eac_pm-row -->
 
 <!--
   Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
--- themes/EaContentPaulsMotors/page.php
+++ themes/EaContentPaulsMotors/page.php
@@ -1,14 +1,102 @@
 <?php
 /**
- * This file defines the basic html structure for the sdmSimple theme.
+ * This file defines the default structure of a page for the EaContent_PaulsMotors theme.
+ *
+ * It was created for Ea Content by Sevi Donnelly Foreman.
+ *
+ * @author: Sevi Donnelly Foreman
+ *
+ * @created: April 27, 2016 at 3:53 p.m.
+ *
  */
 ?>
 
-<div id="main_content">
-    <?php
-    echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
-    ?>
+<!-- #locked_eac_pm-header-row | .eac_pm-row -->
+<div id="locked_eac_pm-header-row" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-header-logo | .eac_pm-col -->
+    <div id="locked_eac_pm-header-logo" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-logo-text -->
+        <div id="locked_eac_pm-header-logo-text">
+            <h1>Paul's Motors</h1>
+        </div>
+        <!-- End #locked_eac_pm-header-logo-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-logo | .eac_pm-col -->
+
+    <!-- #locked_eac_pm-header-address | .eac_pm-col -->
+    <div id="locked_eac_pm-header-address" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-address-text -->
+        <div id="locked_eac_pm-header-address-text">6 Fairview Ave Poughkeepsie Ny</div>
+        <!-- End #locked_eac_pm-header-address-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-address | .eac_pm-col -->
+
+    <!-- #locked_eac_pm-header-phone-number | .eac_pm-col -->
+    <div id="locked_eac_pm-header-phone-number" class="dev eac_pm-col-">
+
+        <!-- #locked_eac_pm-header-phone-number-text -->
+        <div id="locked_eac_pm-header-phone-number-text">
+            <h3>845-471-4240</h3>
+        </div>
+        <!-- End #locked_eac_pm-header-phone-number-text -->
+
+    </div>
+    <!-- End #locked_eac_pm-header-phone-number | .eac_pm-col -->
+
 </div>
+<!-- End #locked_eac_pm-header-row | .eac_pm-row -->
+
+<!-- #locked_eac_pm-row2 | .eac_pm-row -->
+<div id="locked_eac_pm-row2" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-hours -->
+    <div id="locked_eac_pm-hours">M-F: 8AM - 5:15PM SAT-SUN 8AM - NOON</div>
+    <!-- End #locked_eac_pm-hours -->
+
+    <!-- #locked_eac_pm-main-menu -->
+    <div id="locked_eac_pm-main-menu">
+        <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>Cars</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <!-- End #locked_eac_pm-main-menu -->
+
+</div>
+<!-- End #locked_eac_pm-row2 | .eac_pm-row -->
+
+
+<!-- #locked_eac_pm-row3 | .eac_pm-row -->
+<div id="locked_eac_pm-row3" class="dev eac_pm-row">
+
+    <!-- #eac_pm-box1 -->
+    <div id="main_content">
+        <?php
+        echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
+        ?>
+    </div>
+    <!-- End #eac_pm-box1 -->
+
+</div>
+<!-- End #locked_eac_pm-row3 | .eac_pm-row -->
+
+<!-- #locked_eac_pm-footer-row | .eac_pm-row -->
+<div id="locked_eac_pm-footer-row" class="dev eac_pm-row">
+
+    <!-- #locked_eac_pm-footer-text -->
+    <div id="locked_eac_pm-footer-text"></div>
+    <!-- End #locked_eac_pm-footer-text -->
+
+</div>
+<!-- End #locked_eac_pm-footer-row | .eac_pm-row -->
+
 <!--
   Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
   This wrapper is meant to hold core and user app output
