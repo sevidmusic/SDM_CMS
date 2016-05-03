202a7168a6a7bacad6e4d3d015ee312e818b34a1
sevidmusic <sevidmusic@gmail.com>
2016-05-03 10:36:28 -0400

    THEMES: Started developing new theme WindhamWineAndLiquors.

---
+++ themes/WindhamWineAndLiquors/WindhamWineAndLiquors.as
@@ -0,0 +1,3 @@
+stylesheets=css/wwl_layout,css/wwl_forms,css/wwl_menus,css/wwl_formatting,css/wwl_wrappers;
+scripts=none;
+meta=[name:description, content:Website powered by the SDM CMS], [name:author, content:Sevi Donnelly Foreman], [http-equiv:refresh, content:3000], [name:viewport, content:[width=device-width|initial-scale=1.0]];
---
+++ themes/WindhamWineAndLiquors/page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file defines the basic html structure for the sdmResponsive theme.
+ */
+?>
+
+<div id="main_content"
+     class="col-12">
+    <?php
+    echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
+    ?>
+</div>
+
+<!--
+  Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
+  This wrapper is meant to hold core and user app output
+  that is not part of the page. For instance, if an app needs to generate
+  a message to the user this core wrapper is a good place to output the message.
+-->
+<div id="Sdm_Cms_Core_Output">
+    <?php
+    echo $sdmassembler->sdmAssemblerGetContentHtml('Sdm_Cms_Core_Output');
+    ?>
+</div>
