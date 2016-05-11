74d57d27f5dcb2bee0d63f47aaa72b7e3ae1ad9a
sevidmusic <sevidmusic@gmail.com>
2016-05-11 10:44:32 -0400

    User app SdmMediaDisplays: Add display form handler loading successfully.

--- apps/SdmMediaDisplays/admin/formHandlers/selectDisplayPanel_addDisplays.php
+++ apps/SdmMediaDisplays/admin/formHandlers/selectDisplayPanel_addDisplays.php
@@ -6,16 +6,9 @@
  * Time: 1:57 PM
  */
 
+$sdmassembler->sdmCoreSdmReadArray('Add displays form handler loaded successfully.');
 /* Create display directory if it does not already exist. */
 $displayDirectoryPath = $sdmassembler->sdmCoreGetUserAppDirectoryPath() . '/SdmMediaDisplays/displays/data/' . $nameOfDisplayBeingEdited;
 if (is_dir($displayDirectoryPath) === false) {
     $status = mkdir($displayDirectoryPath, 0777);
 }
-
-/* If there the display's directory has media in it load it. */
-if($sdmMediaDisplay->sdmMediaDisplayHasMedia($nameOfDisplayBeingEdited) === true) {
-//var_dump('loading media for ' . $nameOfDisplayBeingEdited . '...');
-}
-
-
-
