25602ebc33169083b9961c552a8b64f9251b5d66
sevidmusic <sevidmusic@gmail.com>
2016-05-11 11:08:27 -0400

    User app SdmMediaDisplays: sdmMediaDisplayHasMedia() working. Prepping to create form elements from current display's media.

--- apps/SdmMediaDisplays/admin/formsElements/mediaList.php
+++ apps/SdmMediaDisplays/admin/formsElements/mediaList.php
@@ -6,4 +6,8 @@
  * Time: 10:49 AM
  */
 
-var_dump('Loading media list form elements...');
+var_dump('loaded media list');
+/* If there the display's directory has media in it load it and build a radio form element for each piece of media. */
+var_dump($nameOfDisplayBeingEdited, $sdmMediaDisplay->sdmMediaDisplayHasMedia($nameOfDisplayBeingEdited));// === true) {
+//var_dump('loading media for ' . $nameOfDisplayBeingEdited . '...');
+//}
