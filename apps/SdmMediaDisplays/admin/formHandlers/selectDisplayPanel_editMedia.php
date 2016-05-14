7508bee8219ebe953227e10f41bf0e91c895af6b
sevidmusic <sevidmusic@gmail.com>
2016-05-13 20:04:43 -0400

    User app SdmMediaDisplays: Submitted edit media form values being unpacked by form handler.

--- apps/SdmMediaDisplays/admin/formHandlers/selectDisplayPanel_editMedia.php
+++ apps/SdmMediaDisplays/admin/formHandlers/selectDisplayPanel_editMedia.php
@@ -8,6 +8,30 @@
 
 if ($adminMode === 'saveMedia') {
 
+    /* Get submitted form values */
+    $submittedEditMediaFormValues = $sdmMediaDisplaysAdminForm->sdmFormGetSubmittedFormValue('all');
+
+    /* Initialize array to hold new media property values. */
+    $newMediaPropertyValues = array();
+
+    /* Add each submitted form value to the $newMediaPropertyValues array. It's ok if values unrelated to the SdmMedia object
+       are included because they will simply be ignored on creation of new SdmMedia() object. */
+    foreach ($submittedEditMediaFormValues as $submittedEditMediaFormKey => $submittedEditMediaFormValue) {
+        $newMediaPropertyValues[$submittedEditMediaFormKey] = $sdmMediaDisplaysAdminForm->sdmFormGetSubmittedFormValue($submittedEditMediaFormKey);
+    }
+
+    /* Create new SdmMedia() object instance. */
+    $updateMediaObject = new SdmMedia();
+
+    /* Create new media object from new media property values. */
+    $newMediaObject = $updateMediaObject->sdmMediaCreateMediaObject($newMediaPropertyValues);
+
+    /* Json encode new media object to prepare for storage. */
+    $newMediaObjectJson = json_encode($newMediaObject);
+
+    /* Save new media object  */
+    var_dump($newMediaObject, $newMediaObjectJson);
+
     /* Added confirmation message to panel description. */
     $panelDescription .= '<p>Saved changes to media "' . $sdmMediaDisplaysAdminForm->sdmFormGetSubmittedFormValue('sdmMediaDisplayName') . '".</p>';
 
