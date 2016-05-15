18f3d20415c0e317914e00fc8acb400985cb1978
sevidmusic <sevidmusic@gmail.com>
2016-05-15 00:24:23 -0400

    User app SdmMediaDisplays:Reworked hompeage display design.

--- apps/SdmMediaDisplays/SdmMediaDisplays.as
+++ apps/SdmMediaDisplays/SdmMediaDisplays.as
@@ -1,1 +1,1 @@
-stylesheets=css/SdmDisplaysAdminPanels;
+stylesheets=css/SdmDisplaysAdminPanels,displays/css/homepage;
--- apps/SdmMediaDisplays/displays/templates/homepage.php
+++ apps/SdmMediaDisplays/displays/templates/homepage.php
@@ -8,15 +8,7 @@
  */
 function defaultSdmMediaDisplayDiv($media)
 {
-    $styles = array(
-        'text-align:center',
-        'margin-bottom:52px',
-        'width: 1142px',
-        'height: 1142px',
-        'padding: 42px',
-        'border: 3px solid #ffffff',
-    );
-    echo '<div style="' . implode('; ', $styles) . '">' . $media . '</div>';
+    echo '<div id="sdmMediaHomepageDisplay"><div class="sdmMediaHomepageDisplayMedia">' . $media . '</div></div>';
 }
 
 echo $sdmMediaDisplay->sdmMediaDisplayGenerateMediaDisplay('defaultSdmMediaDisplayDiv');
