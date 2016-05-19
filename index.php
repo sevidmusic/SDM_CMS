5ea8bfe9d4137f42697307b7441a2aa4e81b4962
sevidmusic <sevidmusic@gmail.com>
2016-05-19 08:35:28 -0400

    CORE index.php: Run clearstatcache() to clear PHP's file cache. The SDM CMS relies on json as it's storage mechanism, which means json files are regularly being read and modified. So, to insure the all information about the file is completely up to date PHP's file cache must be cleared so cached values are never used when handling files. This code was added as a first attempt to fix to a very strange bug that causes reset.php to run randomly. This is very bad of course because it means the whole site was reset, complete data loss!!! So the file cache is cleared so cashed values don't somehow cause reset.php to run. On idea about what specifically causes reset.php to run is: index.php checks that data.json exists, if it doesn't it redirects to reset.php to configure a new Sdm Cms site. The bug may happen if as a result of the file cache, data.json is perceived to be inaccessible causing reset.php to run. Testing still needs to be done to confirm this, but from the logs, research on php.net, and seeing the bug occur a few times this the current  diagnosis. @see http://php.net/manual/en/function.clearstatcache.php for more info.

--- index.php
+++ index.php
@@ -4,6 +4,29 @@
  * Index file for the Sdm Cms. This file serves all site pages.
  */
 
+/* Run clearstatcache() to clear PHP's file cache. The SDM CMS relies on json as it's storage
+ * mechanism, which means json files are regularly being read and modified. So, to insure the
+ * all information about the file is completely up to date PHP's file cache must be cleared so
+ * cached values are never used when handling files.
+ *
+ * This code was added as a first attempt to fix to a very strange bug that causes reset.php to run randomly.
+ * This is very bad of course because it means the whole site was reset, complete data loss!!! So the file cache
+ * is cleared so cashed values don't somehow cause reset.php to run.
+ *
+ * On idea about what specifically causes reset.php to run is:
+ *
+ * index.php checks that data.json exists, if it doesn't it redirects to reset.php to configure
+ * a new Sdm Cms site. The bug may happen if as a result of the file cache, data.json is perceived
+ * to be inaccessible causing reset.php to run. Testing still needs to be done to confirm this,
+ * but from the logs, research on php.net, and seeing the bug occur a few times this the current
+ * diagnosis.
+ *
+ * @see http://php.net/manual/en/function.clearstatcache.php for more info.
+ *
+ */
+
+clearstatcache();
+
 /* If core site components have been not been configured, redirect to reset.php. */
 if (!file_exists(__DIR__ . '/core/sdm/data.json')) {
     $rootUrl = str_replace(array('/index.php', '/reset.php'), '', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
