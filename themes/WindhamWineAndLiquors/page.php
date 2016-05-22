78ac4230ccb9abd3b2975322c88895aafc0ad1bc
sevidmusic <sevidmusic@gmail.com>
2016-05-21 21:53:24 -0400

    Theme Windham Wine And Liquors: Continued development of Windham Wine and Liquors theme.

--- themes/WindhamWineAndLiquors/page.php
+++ themes/WindhamWineAndLiquors/page.php
@@ -1,45 +1,53 @@
 <!--
 Naming conventions for this theme:
 core wrappers: core_wrapper_name | core wrapper names are mixed case, and use underscores for spaces
-locked wrappers : locked_camelCaseWrapperName | locked wrapper names are camel case, and are prefixed with an acronym of the theme name
-unlocked wrappers : wrapper-name | unlocked wrappers are all lower case and use hyphens for spaces.
+locked wrappers : locked_camelCaseWrapperName | locked wrapper names are camel case, and are prefixed"wwl_"
+unlocked wrappers : wrapper-name | unlocked wrapper names are all lower case and use hyphens for spaces.
+classes : wwl_className | classes are camel case and are prefixed with"wwl_"
 -->
+<div id="locked_siteWrapper">
+    <div class="border row">
+        <div id="locked_headerWrapper" class="border wwl_column20">
+            <div id="header"><h1><a href="<?php echo $sdmassembler->sdmCoreGetRootDirectoryUrl(); ?>/index.php">Windham
+                        Wine &amp; Liquors</a></h1></div>
+        </div>
+    </div>
+    <div class="border row">
+        <div id="locked_mainMenuWrapper" class="border wwl_column20">
+            <div id="main-menu" class="border">
+                <?php
+                echo $sdmassembler->sdmAssemblerGetContentHtml('main-menu');
+                ?>
+            </div>
+        </div>
+    </div>
 
-<!-- Basic row structure
-<div class="row">
-    <div class="col-12"></div>
-</div>
--->
+    <div class="border row">
+        <div id="locked_mainContentWrapper" class="border wwl_column20">
+            <div id="main_content">
+                <?php
+                echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
+                ?>
+            </div>
+        </div>
 
-<!--
-Wrapper: #mainMenu
-Wrapper Classes: wwl_menu,
-Child Wrappers: main-menu
--->
-<div id="locked_mainMenuWrapper">
-    <div id="main-menu" class="wwl_mainMenu">
+    </div>
+    <div class="border row">
+        <div id="locked_footerWrapper" class="border wwl_column20">
+            <div id="locked_footer">Windham Wine &amp; Liquors &copy; 2016 All Rights Reserved</div>
+        </div>
+
+    </div>
+
+    <!--
+      Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
+      This wrapper is meant to hold core and user app output
+      that is not part of the page. For instance, if an app needs to generate
+      a message to the user this core wrapper is a good place to output the message.
+    -->
+    <div id="Sdm_Cms_Core_Output">
         <?php
-        echo $sdmassembler->sdmAssemblerGetContentHtml('main-menu');
+        echo $sdmassembler->sdmAssemblerGetContentHtml('Sdm_Cms_Core_Output');
         ?>
     </div>
-</div>
-
-<div class="row">
-    <div id="main_content" class="wwl_col-12">
-        <?php
-        echo $sdmassembler->sdmAssemblerGetContentHtml('main_content');
-        ?>
-    </div>
-</div>
-
-<!--
-  Special wrapper provided by Sdm Cms Core to hold Sdm Cms Core Output
-  This wrapper is meant to hold core and user app output
-  that is not part of the page. For instance, if an app needs to generate
-  a message to the user this core wrapper is a good place to output the message.
--->
-<div id="Sdm_Cms_Core_Output">
-    <?php
-    echo $sdmassembler->sdmAssemblerGetContentHtml('Sdm_Cms_Core_Output');
-    ?>
 </div>
