9be515c628a2b55876b84a65d84ce22fbc84f52f
sevidmusic <sevidmusic@gmail.com>
2016-06-03 11:58:07 -0400

    User App Sdm Media Displays: Media files now being uploaded, and their data is also being saved.

--- apps/SdmMediaDisplays/includes/SdmMediaDisplaysAdmin.php
+++ apps/SdmMediaDisplays/includes/SdmMediaDisplaysAdmin.php
@@ -213,8 +213,12 @@ class SdmMediaDisplaysAdmin extends SdmForm
 
                 break;
             case 'saveMedia':
+                /* Attempt to upload media file. */
                 $fileUploaded = $this->uploadMedia();
-                var_dump('file uploaded: ' . strval($fileUploaded));
+                /* If media was uploaded successfully save media data. */
+                if ($fileUploaded !== false) {
+                    $this->saveMediaData($fileUploaded);
+                }
                 break;
         }
     }
@@ -423,8 +427,132 @@ class SdmMediaDisplaysAdmin extends SdmForm
         /* Log upload. */
         file_put_contents($this->sdmCore->sdmCoreGetUserAppDirectoryPath() . '/SdmMediaDisplays/logs/sdmMediaDisplayLog_' . time() . '.html', $sdmMediaUploadLogMessage);
 
-        return $fileUploadStatus;
+        return ($fileUploadStatus === true ? $uniqueFileName : $fileUploadStatus);
 
+    }
+
+    private function saveMediaData($uniqueFileName)
+    {
+        /* Get submitted form values */
+        $submittedEditMediaFormValues = array_keys($this->sdmFormGetSubmittedFormValue('all'));
+
+        /** Unpack vars from file upload handler **/
+
+        /* The unique file name generated on file upload. This includes the file extension. */
+        $fileName = $uniqueFileName;
+
+        /* Generate a save file name to be used as the sdmMediaSourceName and as the name of the json and media
+           files that are created for this media object. Does not include file extension. */
+        $safeFileName = substr($uniqueFileName, 0, strpos($uniqueFileName, '.'));
+
+        /* Initialize array to hold new media property values. */
+        $newMediaPropertyValues = array();
+
+        /* Add each submitted form value to the $newMediaPropertyValues array. It's ok if values unrelated to the SdmMedia object
+           are included because they will simply be ignored on creation of new SdmMedia() object. */
+        foreach ($submittedEditMediaFormValues as $submittedEditMediaFormKey) {
+            switch ($submittedEditMediaFormKey) {
+                case 'sdmMediaSourceUrl':
+                    /* If sdmMediaSourceType is local, enforce local url, otherwise use supplied. */
+                    $newMediaPropertyValues[$submittedEditMediaFormKey] = ($this->sdmFormGetSubmittedFormValue('sdmMediaSourceType') === 'local' ? $this->sdmCore->sdmCoreGetRootDirectoryUrl() . '/apps/SdmMediaDisplays/displays/media' : $this->sdmFormGetSubmittedFormValue($submittedEditMediaFormKey));
+                    break;
+                case 'sdmMediaProtected':
+                case 'sdmMediaPublic':
+                    /* For now enforce public and protected using false since these properties
+                       have not yet been implemented. */
+                    $newMediaPropertyValues[$submittedEditMediaFormKey] = false;
+                    break;
+                default:
+                    /* Use submitted value without special processing. */
+                    $newMediaPropertyValues[$submittedEditMediaFormKey] = $this->sdmFormGetSubmittedFormValue($submittedEditMediaFormKey);
+                    break;
+            }
+        }
+
+        /* Create new SdmMedia() object instance. */
+        $updateMediaObject = new SdmMedia();
+
+        /* Create new media object from new media property values. */
+        $newMediaObject = $updateMediaObject->sdmMediaCreateMediaObject($newMediaPropertyValues);
+
+        /** Set Properties that rely on file upload handler. **/
+
+        switch ($newMediaPropertyValues['sdmMediaSourceType']) {
+            case 'external':
+                /* Set safe file name */
+                $safeFileName = hash('sha256', $newMediaPropertyValues['sdmMediaSourceUrl']);
+
+                /* Set media source name based on $safeFileName */
+                $newMediaObject->sdmMediaSetSourceName($safeFileName);
+
+                /* Set media source id based on uploaded file name */
+                $newMediaObject->sdmMediaSetId($safeFileName);
+
+                /* Encode external media source urls */
+                $this->sdmMediaEncodeExternalMediaUrl();
+
+                /* Properly encode embed urls for display */
+                $newMediaObject->sdmMediaSetSourceUrl(sdmMediaEncodeExternalMediaUrl($newMediaPropertyValues['sdmMediaSourceUrl']));
+
+                break;
+            default:
+
+                /* Set media source name based on uploaded file name */
+                $newMediaObject->sdmMediaSetSourceName($safeFileName);
+
+                /* Set media source id based on uploaded file name */
+                $newMediaObject->sdmMediaSetId($safeFileName);
+                break;
+        }
+
+        /* Set media source extension based on uploaded file name | @todo: validate agaisnt a whitelist of valid extensions. */
+        $fileExtension = substr($fileName, strpos($fileName, ".") + 1);
+        $newMediaObject->sdmMediaSetSourceExtension($fileExtension);
+
+        /* Convert media display name to camel case and set as machine name */
+        $camelCaseMediaName = str_replace(' ', '', ucfirst(preg_replace("/[^a-z]+/i", "", $this->sdmFormGetSubmittedFormValue('sdmMediaDisplayName'))));
+
+        /* Set  media machine name */
+        $newMediaObject->sdmMediaSetMachineName($camelCaseMediaName);
+
+        /* Json encode new media object to prepare for storage. */
+        $newMediaObjectJson = json_encode($newMediaObject);
+
+        /* Save new media object  */
+        file_put_contents($this->sdmMediaDisplaysDataDirectoryPath . '/' . $this->displayBeingEdited . '/' . $newMediaObject->sdmMediaGetSdmMediaSourceName() . '.json', $newMediaObjectJson);
+
+        /* Added confirmation message to panel description. */
+        $this->output .= '<p>Saved changes to media "' . $this->sdmFormGetSubmittedFormValue('sdmMediaDisplayName') . '".</p>';
+    }
+
+    /**
+     * Attempts to encode an external media url so it complies to the embed format
+     * of the provider. At the moment this function only supports Vimeo, and Youtube.
+     * Other prover urls will be returned without modification.
+     *
+     * @param string $url The external media url to encode
+     *
+     * @return string The encoded url.
+     */
+    private function sdmMediaEncodeExternalMediaUrl($url = 'https://www.youtube.com/watch?v=brj_cFsDc7Y')
+    {
+        /* Determine provider. */
+        $provider = parse_url($url);
+
+        /* Encode url based on provider */
+        switch ($provider['host']) {
+            case 'youtube.com':
+            case 'www.youtube.com':
+                $embedUrl = str_replace('watch?v=', 'embed/', $url);
+                break;
+            case 'vimeo.com':
+                $embedUrl = str_replace('vimeo.com/', 'player.vimeo.com/video/', $url);
+                break;
+            default:
+                $embedUrl = $url;
+                break;
+        }
+        return $embedUrl;
     }
 
     public function getCurrentAdminPanel()
