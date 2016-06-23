ef32524b96b000020bca1c5800ff36c80208fb91
sevidmusic <sevidmusic@gmail.com>
2016-06-22 23:30:06 -0400

    User App Sdm Media Displays: Moved logic used to determine available displays into a new function., determineAvailableDisplays(). The reason it was not moved into one of the Sdm Media Display classes is because the logic in this new function is used by various Sdm Media Display components and the issue of inheritance and injection would have complicated things if the logic was placed into a method of only one of the Sdm Media Display classes. Now if any component of the Sdm Media displays app needs to determine which displays are available, they can simply call the new determineAvailableDisplays() function.

--- apps/SdmMediaDisplays/SdmMediaDisplays.php
+++ apps/SdmMediaDisplays/SdmMediaDisplays.php
@@ -6,23 +6,46 @@
  * Time: 5:59 PM
  */
 
-/* If the data directory exists, attempt to load any displays that exist. */
-if (is_dir(__DIR__ . '/displays/data') === true) {
+/**
+ * Returns an array of available displays.
+ *
+ * @param sdmAssembler $sdmAssembler Instance of the sdmAssembler() class.
+ *
+ * @return array Returns an array of available displays. If there are no available displays this function will return an empty array.
+ */
+function determineAvailableDisplays(sdmAssembler $sdmAssembler)
+{
     /* Get a directory listing of available displays. */
-    $displaysDirectoryListing = $sdmassembler->sdmCoreGetDirectoryListing('SdmMediaDisplays/displays/data', 'apps');
+    $displaysDirectoryListing = $sdmAssembler->sdmCoreGetDirectoryListing('SdmMediaDisplays/displays/data', 'apps');
+
+    /* Initialize $availableDisplays array. */
     $availableDisplays = array();
+
+    /* Create an array of directories that should be ignored, i.e. array of directories that are known to not represent a display. */
     $ignoredListings = array('.', '..', '.DS_Store');
+
+    /* Add listings not in the $ignoredListings array to the available displays array. */
     foreach ($displaysDirectoryListing as $availableDisplay) {
         if (in_array($availableDisplay, $ignoredListings) === false) {
             $availableDisplays[$availableDisplay] = $availableDisplay;
         }
     }
 
+    /* Return array of available displays */
+    return $availableDisplays;
+}
+
+/* If the data directory exists, attempt to load any displays that exist. */
+if (is_dir(__DIR__ . '/displays/data') === true) {
+
+    /* Determine available displays */
+    $availableDisplays = determineAvailableDisplays($sdmassembler);
+
+    /* Process each available display. */
     foreach ($availableDisplays as $currentDisplay) {
 
         /* Create an instance of SdmCore() for the SdmMediaDisplay(). */
         $SdmCore = new SdmCore();
-
 
         /* Create New Sdm Media Display */
         $sdmMediaDisplay = new SdmMediaDisplay($currentDisplay, $SdmCore);
